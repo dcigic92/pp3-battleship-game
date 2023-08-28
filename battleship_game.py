03ba1407c8cdc3bc8c7a028bf0fabc813bdb5b00
dcigic92 <dinocigic@yahoo.com>
2023-08-28 02:39:05 +0100

    Add validation to main function

--- battleship_game.py
+++ battleship_game.py
@@ -1,7 +1,10 @@
 
 class BattleshipGame:
-    def __init__(self, rows, cols, ships):
+    def __init__(self, rows, cols, num_ships):
         self.rows = rows
         self.cols = cols
-        self.user_ships = ships
-        self.computer_ships = ships
+        self.user_ships = num_ships
+        self.computer_ships = num_ships
+
+    def play(self):
+        pass
--- run.py
+++ run.py
@@ -2,12 +2,39 @@ from battleship_game import BattleshipGame
 
 
 def main():
+    while True:
+        try:
+            rows = int(input("Enter the number of rows (between 5 and 8): "))
+            if 5 <= rows <= 8:
+                break
+            else:
+                raise ValueError(f"Number of rows must be between 5 and 8, you entered number {rows}")
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please enter a valid number.")
 
-    rows = int(input("Enter the number of rows: "))
-    cols = int(input("Enter the number of columns: "))
-    ships = int(input("Enter the number of ships: "))
-    game = BattleshipGame(rows, cols, ships)
+    while True:
+        try:
+            cols = int(input("Enter the number of columns (between 5 and 8): "))
+            if 5 <= cols <= 8:
+                break
+            else:
+                raise ValueError(f"Number of columns must be between 5 and 8, you entered number {cols}")
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please enter a valid number.")
+
+    while True:
+        try:
+            num_ships = int(input("Enter the number of ships (4 or 5): "))
+            if num_ships in [4, 5]:
+                break
+            else:
+                raise ValueError(f"Number of ships must be either 4 or 5, you entered number {num_ships}")
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please enter a valid number.")
+
+    game = BattleshipGame(rows, cols, num_ships)
+    game.play()
 
 
-print("Welcome to Battleship Game")
+print("Welcome to the Battleship Game")
 main()
