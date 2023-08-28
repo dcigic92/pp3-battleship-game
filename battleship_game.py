48d04761d12b6526088b40200fa061387f6cc533
dcigic92 <dinocigic@yahoo.com>
2023-08-28 11:59:12 +0100

    Set up structure of game functions

--- battleship_game.py
+++ battleship_game.py
@@ -11,6 +11,21 @@ class BattleshipGame:
         self.user_battlefield = BattlefieldGenerator.generate_battlefield(rows, cols)
         self.computer_battlefield = BattlefieldGenerator.generate_battlefield(rows, cols)
 
+    def user_place_ships(self):
+        pass
+
+    def computer_place_ships(self):
+        pass
+
+    def get_valid_coordinates(self):
+        pass
+
+    def user_attack(self):
+        pass
+
+    def computer_attack(self):
+        pass
+
     def play(self):
         print(self.user_battlefield)
         print(self.computer_battlefield)
